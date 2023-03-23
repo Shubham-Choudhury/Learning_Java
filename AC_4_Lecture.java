8fc17b9ec1672c35343aa84351ee721a18c91744
Shubham Choudhury <shubhamchoudhury2002@yahoo.com>
2023-03-23 06:43:57 +0530

    Lecture 4

---
+++ AC_4_Lecture.java
@@ -0,0 +1,116 @@
+public class AC_4_Lecture {
+    public static void main(String[] args) {
+        // ==================== LOOPS ==================== \\
+        // Loops are used to repeat a block of code.
+        // There are 3 types of loops in Java:
+        // 1. for loop
+        // 2. while loop
+        // 3. do-while loop
+
+        /*
+         * ==================== FOR LOOP ====================
+         * for (initialization; condition; increment/decrement) {
+         * // code
+         * }
+         * The initialization is executed only once, when the loop starts.
+         * The condition is evaluated at the beginning of each loop iteration.
+         * The code block will be executed as long as the condition is true.
+         * The increment/decrement is executed at the end of each loop iteration.
+         * The increment/decrement is optional.
+         */
+        System.out.println("=================== FOR LOOP =================");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hello World");
+        }
+        System.out.println("====================================");
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Hello World - " + i);
+            if (i == 3) {
+                break;
+            }
+        }
+        System.out.println("====================================");
+        for (int i = 1; i < 6; i++) {
+            if (i == 3) {
+                continue;
+            }
+            System.out.println("Hello World - " + i);
+        }
+
+        /*
+         * ==================== WHILE LOOP ====================
+         * while (condition) {
+         * // code
+         * }
+         * The condition is evaluated at the beginning of each loop iteration.
+         * The code block will be executed as long as the condition is true.
+         */
+        System.out.println("=================== WHILE LOOP =================");
+        int i = 0;
+        while (i < 3) {
+            System.out.println("Hello World");
+            i++;
+        }
+        System.out.println("====================================");
+        i = 1;
+        while (i < 6) {
+            System.out.println("Hello World - " + i);
+            if (i == 3) {
+                break;
+            }
+            i++;
+        }
+        System.out.println("====================================");
+        i = 1;
+        while (i < 6) {
+            if (i == 3) {
+                i++;
+                continue;
+            }
+            System.out.println("Hello World - " + i);
+            i++;
+        }
+
+        /*
+         * ==================== DO-WHILE LOOP ====================
+         * do {
+         * // code
+         * } while (condition);
+         * The code block will be executed at least once, even if the condition is
+         * false.
+         * The condition is evaluated at the end of each loop iteration.
+         */
+        System.out.println("=================== DO-WHILE LOOP =================");
+        i = 0;
+        do {
+            System.out.println("Hello World");
+            i++;
+        } while (i < 3);
+        System.out.println("====================================");
+        i = 1;
+        do {
+            System.out.println("Hello World - " + i);
+            if (i == 3) {
+                break;
+            }
+            i++;
+        } while (i < 6);
+        System.out.println("====================================");
+        i = 1;
+        do {
+            if (i == 3) {
+                i++;
+                continue;
+            }
+            System.out.println("Hello World - " + i);
+            i++;
+        } while (i < 6);
+        System.out.println("====================================");
+        i = 1;
+        do {
+            System.out.println("Hello Duniya");
+            i++;
+        } while (i == 0);
+
+    }
+}
--- README.md
+++ README.md
@@ -15,21 +15,29 @@
             <td>Lecture 1</td>
             <td>Introduction to Java</td>
             <td><a href="https://www.youtube.com/watch?v=yRpLlJmRo2w">Watch</a></td>
-            <td></td>
+            <td><a href="https://github.com/Shubham-Choudhury/Learning_Java/blob/main/AC_1_Lecture.java">Code</a></td>
             <td></td>
         </tr>
         <tr>
             <td>Lecture 2</td>
             <td>Input/Output Variable & Data Types</td>
             <td><a href="https://www.youtube.com/watch?v=LusTv0RlnSU">Watch</a></td>
-            <td></td>
+            <td><a href="https://github.com/Shubham-Choudhury/Learning_Java/blob/main/AC_2_Lecture.java">Code</a></td>
             <td></td>
         </tr>
         <tr>
             <td>Lecture 3</td>
             <td>Conditional Statements: If-else, Switch Break</td>
             <td><a href="https://www.youtube.com/watch?v=I5srDu75h_M&t=596s">Watch</a></td>
+            <td><a href="https://github.com/Shubham-Choudhury/Learning_Java/blob/main/AC_3_Lecture.java">Code</a></td>
             <td></td>
+        </tr>
+        <tr>
+            <td>Lecture 4</td>
+            <td>Loops: For, While, Do-While</td>
+            <td><a href="https://www.youtube.com/watch?v=0r1SfRoLuzU">Watch</a></td>
+            <td><a href="https://github.com/Shubham-Choudhury/Learning_Java/blob/main/AC_4_Lecture.java">Code</a></td>
             <td></td>
+        </tr>
     </tbody>
 </table>
